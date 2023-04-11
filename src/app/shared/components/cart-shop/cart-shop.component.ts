3d31187c38e2619bca6455fac8eafe47b22229bc
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-11 15:35:49 -0500

    implmentando carrito funcional con items

--- src/app/shared/components/cart-shop/cart-shop.component.ts
+++ src/app/shared/components/cart-shop/cart-shop.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart-shop',
   templateUrl: './cart-shop.component.html',
   styleUrls: ['./cart-shop.component.css']
 })
-export class CartShopComponent {
+
+export class CartShopComponent implements OnInit {
+
+  itemsCart: { cartItems: Array<any> }
+  = { cartItems: [] }
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+    this.itemsCart.cartItems = [
+      {
+        img: '/assets/logo.svg',
+        descripcion: 'Aquí la descripción del producto',
+        cantidad: 1,
+        precio: 300,
+      },
+      {
+        img: '/assets/logo.svg',
+        descripcion: 'Aquí la descripción del producto',
+        cantidad: 1,
+        precio: 300,
+      },
+      {
+        img: '/assets/logo.svg',
+        descripcion: 'Aquí la descripción del producto',
+        cantidad: 1,
+        precio: 300,
+      },
+
+    ]
+
+  }
+
+
 
 }
