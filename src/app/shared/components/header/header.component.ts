3213450d837d6c0d4564253ec4cb83f9c69c6f28
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-19 13:59:28 -0500

    header ngfor implementada faltan routes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  
+  accesLinks: Array<any> = [];
+
+  constructor(private router: Router) {}
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.accesLinks = [
+      {
+        title: 'Home',
+        router: ["/"]
+      },
+      { 
+        title: 'Contact',
+        router: ['/contact']
+      },
+      { 
+        title: 'Products',
+        router: ['/products']
+      },
+      { 
+        title: 'About Us',
+        router: ['/about']
+      }
+    ]
   }
 
 }
