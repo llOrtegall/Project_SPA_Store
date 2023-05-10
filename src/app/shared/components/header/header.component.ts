9ec3fbed973b7e6a3230a009c57b0ef1d959e57a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-10 08:10:05 -0500

    header terminada

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  
+export class HeaderComponent implements OnInit {
+
+  nameCompany: string = 'COMPANY NAME';
+
+  LinksNav: Array<any> = [];
+  LinkSocial: Array<any> = [];
+
+  ngOnInit(): void {
+    this.LinksNav = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate'
+      },
+      {
+        name: 'About',
+        icon: 'uil uil-cloud-info'
+      },
+      {
+        name: 'Products',
+        icon: 'uil uil-shopping-cart'
+      },
+      {
+        name: 'Contact',
+        icon: 'uil uil-at'
+      },
+    ];
+
+    this.LinkSocial = [
+      {
+        icon: 'uil uil-facebook-f'
+      },
+      {
+        icon: 'uil uil-instagram'
+      },
+      {
+        icon: 'uil uil-youtube'
+      }
+    ]
+
+  }
 }
