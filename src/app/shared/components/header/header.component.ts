95be2d93ec0a51ed022841b44ffb4b27c5ed4647
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-10 07:21:30 -0500

    creando navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor(private router: Router) {}
-  ngOnInit(): void {}
-
+export class HeaderComponent {
+  
 }
