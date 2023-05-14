f3206a97ac12b239c3c9ed03c15de3550a7f9ea5
llOrtegall <IvanOrtega_97@hotmail.com>
2023-05-13 20:29:16 -0500

    de nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeModule } from './modules/home/home.module';
-import { MainPageComponent } from './modules/home/main-page/main-page.component';
 
-const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import(`./modules/auth/auth.module`).then( m => m.AuthModule)
-  },
-  {
-    path: '',
-    component: MainPageComponent,
-    loadChildren: () => import(`./modules/home/home.module`).then( m => m.HomeModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
