61a904986b247a268acfa7519e11ab7461bd53c0
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-19 07:41:48 -0500

    continuar con el proyect

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <nav>
+      <ul className="flex justify-between p-4">
+        <li><a href="">Home</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href=""></a>About</li>
+        <li><a href="">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
